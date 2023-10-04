0e4681659ee707c1b02f2411ce61790018d5d251
Mego <70282836+mobahaa2001@users.noreply.github.com>
2023-10-04 06:12:17 +0300

    Update Solution.java

--- 706. Design HashMap/Solution.java
+++ 706. Design HashMap/Solution.java
@@ -1,48 +1,45 @@
 class MyHashMap {
-
   /** Initialize your data structure here. */
-  Integer[][] map;
-  int NUM_OF_BUCKETS = 1000;
-  int BUCKET_SIZE = 1000;
   public MyHashMap() {
-    map = new Integer[NUM_OF_BUCKETS][BUCKET_SIZE];
+    lists = new List[kSize];
+
+    for (int i = 0; i < kSize; ++i)
+      lists[i] = new ArrayList<>();
   }
 
   /** value will always be non-negative. */
   public void put(int key, int value) {
-    Integer[] bucket = getBucket(key);
-    int keyHash = getKeyHash(key);
-    bucket[keyHash] = value;
+    for (int[] pair : lists[key % kSize])
+      if (pair[0] == key) {
+        pair[1] = value;
+        return;
+      }
+    lists[key % kSize].add(new int[] {key, value});
   }
 
-  /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+  /**
+   * Returns the value to which the specified key is mapped, or -1 if this map
+   * contains no mapping for the key
+   */
   public int get(int key) {
-    Integer[] bucket = getBucket(key);
-    int keyHash = getKeyHash(key);
-    return bucket[keyHash] == null ? -1 : bucket[keyHash];
+    for (int[] pair : lists[key % kSize])
+      if (pair[0] == key)
+        return pair[1];
+    return -1;
   }
 
-  /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+  /**
+   * Removes the mapping of the specified value key if this map contains a mapping
+   * for the key
+   */
   public void remove(int key) {
-    Integer[] bucket = getBucket(key);
-    int keyHash = getKeyHash(key);
-    bucket[keyHash] = null;
+    for (int i = 0; i < lists[key % kSize].size(); ++i)
+      if (lists[key % kSize].get(i)[0] == key) {
+        lists[key % kSize].remove(i);
+        return;
+      }
   }
-  
-  private Integer[] getBucket(int key) {
-    int bucketIdx = key / BUCKET_SIZE;
-    return map[bucketIdx];
-  }
-  
-  private Integer getKeyHash(int key) {
-    return key % BUCKET_SIZE;
-  }
-}
 
-/**
- * Your MyHashMap object will be instantiated and called as such:
- * MyHashMap obj = new MyHashMap();
- * obj.put(key,value);
- * int param_2 = obj.get(key);
- * obj.remove(key);
- */
+  private static final int kSize = 10000;
+  List<int[]>[] lists; // Each slot store (key, value) list
+}
